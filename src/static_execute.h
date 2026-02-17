724f57e25f104c6c162be32faf863c909ce53be9
Carsten Burgard <cburgard@cern.ch>
2026-02-17 18:22:35 +0100

    static execute added

---
+++ src/static_execute.h
@@ -0,0 +1,22 @@
+#ifndef static_execute_h
+#define static_execute_h
+
+// Execute code on library loading by running code in the constructor of a
+// class that is a static class member of another class.
+#define STATIC_EXECUTE(MY_FUNC)   \
+namespace {                       \
+   struct StaticExecutorWrapper { \
+      struct Executor {           \
+         template <class Func>    \
+         Executor(Func func)      \
+         {                        \
+            func();               \
+         }                        \
+      };                          \
+      static Executor executor;   \
+   };                             \
+                                  \
+   StaticExecutorWrapper::Executor StaticExecutorWrapper::executor{MY_FUNC}; \
+}
+
+#endif
